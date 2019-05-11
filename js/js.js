0ead9361c7d939a9068a2053f3e8b6baa870b556
Susank Aryal <jackreacher659@gmail.com>
2019-05-11 08:56:10 +0545

    Job page added and margine and other setting adjusted

---
+++ js/js.js
@@ -0,0 +1,26 @@
+function validateform(fm){
+	if (fm.Firstname.value == "") {
+		alert("Please enter your Firstname.");
+		return false;
+	}
+	if (fm.Lastname.value == "") {
+		alert("Please enter your Lastname.");
+		return false;
+	}
+	if (fm.Email.value == "") {
+		alert("Please enter your Email.");
+		return false;
+    }
+    if (fm.Firstname.value == "") {
+		alert("Please enter your Firstname.");
+		return false;
+	}
+	if (fm.Lastname.value == "") {
+		alert("Please enter your Lastname.");
+		return false;
+	}
+	if (fm.Email.value == "") {
+		alert("Please enter your Email.");
+		return false;
+	}
+}
