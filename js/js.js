000cfa135da2c067cd061968f419d9df9dd468bc
Susank Aryal <jackreacher659@gmail.com>
2019-05-11 18:57:57 +0545

    origin master

--- js/js.js
+++ js/js.js
@@ -11,16 +11,24 @@ function validateform(fm){
 		alert("Please enter your Email.");
 		return false;
     }
-    if (fm.Firstname.value == "") {
-		alert("Please enter your Firstname.");
+    if (fm.country.value == "") {
+		alert("Please enter your country name.");
 		return false;
 	}
-	if (fm.Lastname.value == "") {
-		alert("Please enter your Lastname.");
+	if (fm.local-address.value == "") {
+		alert("Please enter your Local Address.");
 		return false;
 	}
-	if (fm.Email.value == "") {
-		alert("Please enter your Email.");
+	if (fm.permanent-address.value == "") {
+		alert("Please enter your Permanent address.");
+		return false;
+	}
+	if (fm.purpose-for-applying.value == "") {
+		alert("Please enter what post you are applying for.");
+		return false;
+	}
+	if (fm.current-job.value == "") {
+		alert("Please type your current job if none then type 'NONE'");
 		return false;
 	}
 }
